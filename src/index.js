3f6d33efb0493c524e83b42652a7de3de41951ee
Oleksandra Peleno <alexandragubar@gmail.com>
2021-03-06 01:06:19 +0200

    Add theme customization

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './sass/main.scss';
 import refs from './js/refs';
 import './js/up-btn';
 import './js/header-position';
+import './js/storage';
+import './js/themes';
+import './js/theme-change';
 
 import headerRenderMarcup from './js/header-render';
 
---
+++ src/js/themes.js
@@ -0,0 +1,8 @@
+const Themes = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const { LIGHT, DARK } = Themes;
+
+export { LIGHT, DARK };
