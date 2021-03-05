c32d0e2c798f706dd93adff5ed6694043ccd2a09
OksanaStezhko <oksana_stegko@ukr.net>
2021-03-05 15:20:17 +0200

    основы api

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,7 @@ const refs = {
   headerRef: document.querySelector('.header'),
   paginationBox: document.querySelector('.pagination'),
   paginationList: document.querySelector('.pagination__list'),
+  filmListRef: document.querySelector('.films__list'),
 };
 
 export default refs;
