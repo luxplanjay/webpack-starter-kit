3e6a5180149211560561e36eced54a66aafbb041
IvettaDashkova <68033261+IvettaDashkova@users.noreply.github.com>
2021-03-05 22:55:36 +0200

    Ivetta errors (#12)
    * fix: add handler new error feat: add transformation genres_id to genres
    
    * fix: genres string

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,67 +1,94 @@
 //подключение axios (надо загрузить в проект)
 import axios from 'axios';
 
-//массив жанров с их идентификаторами
-const genres = [
-   {id: 28, name: "Action"},
-{id: 12, name: "Adventure"},
-{id: 16, name: "Animation"},
-{id: 35, name: "Comedy"},
-{id: 80, name: "Crime"},
-{id: 99, name: "Documentary"},
-{id: 18, name: "Drama"},
-{id: 10751, name: "Family"},
-{id: 14, name: "Fantasy"},
-{id: 36, name: "History"},
-{id: 27, name: "Horror"},
-{id: 10402, name: "Music"},
-{id: 9648, name: "Mystery"},
-{id: 10749, name: "Romance"},
-{id: 878, name: "Science Fiction"},
-{id: 10770, name: "TV Movie"},
-{id: 53, name: "Thriller"},
-{id: 10752, name: "War"},
-{id: 37, name: "Western"}
-]
 // данные для запроса
 const token = "6b8ef447c2ce3d010bfcc7f710d71588";
 let page = 1;
 const baseURL = `api_key=${token}&page=${page}`;
+const popularMoviesURL = `https://api.themoviedb.org/3/trending/movie/day?${baseURL}`;
+
+//массив жанров с их идентификаторами
+const genres = {
+    28: "Action",
+ 12: "Adventure",
+ 16: "Animation",
+ 35: "Comedy",
+ 80: "Crime",
+ 99: "Documentary",
+ 18: "Drama",
+ 10751: "Family",
+ 14: "Fantasy",
+ 36: "History",
+ 27: "Horror",
+ 10402: "Music",
+ 9648: "Mystery",
+ 10749: "Romance",
+ 878: "Science Fiction",
+ 10770: "TV Movie",
+ 53: "Thriller",
+ 10752: "War",
+ 37: "Western"
+}
+ 
+
 
 //форма поиска и слушатель на ней
-const inputSearch = document.querySelector('#search-form');
-inputSearch.addEventListener('submit', event => onSearch(event));
+const inputSearch = document.querySelector('.search__input');
+inputSearch.addEventListener('input', onSearch);
+
+//предупредительное сообщение об ошибке
+const errorWarning = document.querySelector('.search__warning');
+const message = {
+    manyMatches: 'Too many matches found. Please enter a more specific query!',
+    notFound: 'No results were found for your search',
+    incorrectQuery: 'You entered an incorrect movie name',
+    serverError: 'An error occurred on the server during processing. Please try again later',
+}
 
 //базовая функция запроса списка фильмов
 const fetchFilms = async (moviesURL) => {
     const response = await axios.get(moviesURL)
     .then(({data : {results}}) => {
          if(results.length === 0) {
-             console.log("По Вашему запросу ничего не найдено");
+             errorWarning.textContent = message.notFound;
              return;
          }
-        console.log(results);
+        const changeGenre = [...results].map(el => genresMovie(el));
+        console.log(changeGenre);
         page += 1;
-        return results
+    return changeGenre;
 })
     .catch (error => {
         if(error.response.status === 422) {
-            console.log(error);
-            console.log("Вы ввели некорректное название фильма");
-        } else {
+            errorWarning.textContent = message.incorrectQuery;
+        } 
+        if(error.response.status >= 500) {
+            errorWarning.textContent = message.serverError;
+        } 
+        else {
         console.log(error)}
     })
 }
-
+//преобразование id жанров в названия
+function genresMovie(element) {
+    element.genre_ids = element.genre_ids.map(genreMovie => genreMovie = genres[genreMovie]).join(',');
+    return element;
+}
 //функция поиска по ключевому слову
-function onSearch(event){
-    event.preventDefault();
-    const form = event.currentTarget;
-    if(form.elements.query.value.length >= 1){
-    let searchQuery = form.elements.query.value;
+function onSearch(){
+    errorWarning.textContent = '';
+    if(inputSearch.value.length === 0){
+        fetchFilms(popularMoviesURL);
+    }
+    if(inputSearch.value.length > 0 && inputSearch.value.length < 3){
+        errorWarning.textContent = message.manyMatches;
+    }
+    if(inputSearch.value.length >= 3){
+    let searchQuery = inputSearch.value.trim();
     const searchMoviesURL = `https://api.themoviedb.org/3/search/movie?${baseURL}&query=${searchQuery}`;
     fetchFilms(searchMoviesURL);
-}}
+}
+}
 
 //функция запроса информации о фильме
 const fetchInfoFilm = async (movieID) => {
@@ -72,5 +99,6 @@ const movieID = 512896;
 fetchInfoFilm(movieID);
 
 //стартовый запрос популярных фильмов
-const popularMoviesURL = `https://api.themoviedb.org/3/trending/movie/day?${baseURL}`;
-    fetchFilms(popularMoviesURL);
+   fetchFilms(popularMoviesURL);
+   
+  
