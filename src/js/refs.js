23594430202020117ca0d1b27c18fcba8a4aae2c
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2021-03-05 23:24:40 +0200

    refs added, is-hidden class added onclick myLibrary

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+  input: document.querySelector('#searchQuery'),
+  myLibrary: document.querySelector('#mylibrary'),
+  home: document.querySelector('#home-lib'),
+  watched: document.querySelector('#watched'),
+  queue: document.querySelector('#queue'),
+};
+
+export default refs;
