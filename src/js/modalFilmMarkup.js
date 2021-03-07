3e256416cf1c62555df4484e346b4518cc28ea3d
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-07 11:02:03 +0200

    added removeEventListener on close modal

--- src/js/modalFilmMarkup.js
+++ src/js/modalFilmMarkup.js
@@ -1,36 +1,34 @@
-import * as basicLightbox from 'basiclightbox'
+import * as basicLightbox from 'basiclightbox';
 import 'basicLightbox/src/styles/main.scss';
-import updateModal from './updateModal'
-import searchMovie from './apiFilmFetch'
-import '../scss/styles.scss';
+import updateModal from './updateModal';
+import searchMovie from './apiFilmFetch';
+// ('../scss/styles.scss');
 
-function getFilmInfo(movie_id) {    
-searchMovie(movie_id).then((film) => {
-        const cartFilm = updateModal(film)
-        const modal = basicLightbox.create(cartFilm);
-        modal.onclick = modal.show();
-        window.addEventListener('keydown', closeModalByEscape)
-        function closeModalByEscape(event)
-        {
-            if (event.code === 'Escape')
-            {
-                modal.close()
-            }
-        }
-    })
-}
- 
-const lightBox = () => {
-    let cardIdRef = document.querySelector('.movie-grid');
-    cardIdRef.addEventListener('click', openModal)
-    function openModal(event) {
-        event.preventDefault();
-        if (event.target.nodeName !== "IMG" && event.target.nodeName !== "H2")
-        {
-            return;
-        }
-        getFilmInfo(event.target.dataset.id);
+function getFilmInfo(movie_id) {
+  searchMovie(movie_id).then(film => {
+    const cartFilm = updateModal(film);
+    const modal = basicLightbox.create(cartFilm);
+    modal.onclick = modal.show();
+    window.addEventListener('keydown', closeModalByEscape);
+    function closeModalByEscape(event) {
+      if (event.code === 'Escape') {
+        modal.close();
+        window.removeEventListener('keydown', closeModalByEscape);
+      }
     }
+  });
 }
-    
+
+const lightBox = () => {
+  let cardIdRef = document.querySelector('.movie-grid');
+  cardIdRef.addEventListener('click', openModal);
+  function openModal(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG' && event.target.nodeName !== 'H2') {
+      return;
+    }
+    getFilmInfo(event.target.dataset.id);
+  }
+};
+
 export default lightBox;
