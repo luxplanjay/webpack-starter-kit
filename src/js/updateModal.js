95752dc091b20bd7b8df00f98cb1fc17b0975387
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-07 11:02:30 +0200

    comment stroke

--- src/js/updateModal.js
+++ src/js/updateModal.js
@@ -1,8 +1,8 @@
-'../scss/styles.scss';
-import modalTpl from "../templates/modal.hbs"
+// '../scss/styles.scss';
+import modalTpl from '../templates/modal.hbs';
 
 function updateModal(film) {
-  const markup = modalTpl(film);  
+  const markup = modalTpl(film);
   return markup;
 }
 export default updateModal;
