0b7cbb50335df01c0e441ae22470a913e39fdeb4
Olha Pylypova <olhasve@gmail.com>
2021-03-07 16:09:30 +0200

    fix: close modal by click on cross

--- src/js/modalFilmMarkup.js
+++ src/js/modalFilmMarkup.js
@@ -9,25 +9,34 @@ function getFilmInfo(movie_id) {
     const modal = basicLightbox.create(cartFilm);
     modal.onclick = modal.show();
     window.addEventListener('keydown', closeModalByEscape);
-    function closeModalByEscape(event) {
-      if (event.code === 'Escape') {
-        modal.close();
-        window.removeEventListener('keydown', closeModalByEscape);
+      function closeModalByEscape(event) {
+          if (event.code === 'Escape') {
+              modal.close();
+              window.removeEventListener('keydown', closeModalByEscape);
+          }
       }
-    }
+      const crossRef = document.querySelector('.icon-close');
+        console.log(crossRef);
+      crossRef.addEventListener('click', btnClosedModal)
+      function btnClosedModal() { modal.close()
+        crossRef.removeEventListener('click', btnClosedModal);}  
   });
+       
 }
 
 const lightBox = () => {
   let cardIdRef = document.querySelector('.movie-grid');
   cardIdRef.addEventListener('click', openModal);
-  function openModal(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG' && event.target.nodeName !== 'H2') {
-      return;
+    function openModal(event) {
+        event.preventDefault();
+        if (event.target.nodeName !== 'IMG' && event.target.nodeName !== 'H2') {
+            return;
+        }
+        console.log("event.target.dataset.id=", event.target.dataset.id)
+        getFilmInfo(event.target.dataset.id);
+    
     }
-    getFilmInfo(event.target.dataset.id);
-  }
+     
 };
-
+ 
 export default lightBox;
