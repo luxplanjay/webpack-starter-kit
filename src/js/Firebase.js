45f2f6ea27a75ac3dce8d57a25790f841917b4c0
Valentyn Digtiarenko <baggins999@gmail.com>
2021-03-08 22:13:41 +0200

    commit 1
    Добавил метод addToQueue

--- src/js/Firebase.js
+++ src/js/Firebase.js
@@ -1,5 +1,5 @@
  export class Server {
-     static addWatched(movieId) {
+     static addToWatched(movieId) {
         return fetch(
            'https://movieapi-f5a1d-default-rtdb.firebaseio.com/watched.json',
            {
@@ -9,5 +9,19 @@
                    'Content-Type': 'aplication/json'
                }          
             }).then(response => response.json()).then(data => console.log(data))
-    }
+     }
+     static addToQueue(movieId) {
+         return fetch(
+           'https://movieapi-f5a1d-default-rtdb.firebaseio.com/queue.json',
+           {
+             method: 'POST',
+             body: JSON.stringify(movieId),
+             headers: {
+               'Content-Type': 'aplication/json',
+             },
+           },
+         )
+           .then(response => response.json())
+           .then(data => console.log(data));
+     }
 }
