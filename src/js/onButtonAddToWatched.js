8039659e989a9a1cbc9d4ac7aab131f936effe71
Maryna Kipenko <maryna.i.kipenko@gmail.com>
2021-03-08 18:36:22 +0200

    console deleted, import added

--- src/js/onButtonAddToWatched.js
+++ src/js/onButtonAddToWatched.js
@@ -1,26 +1,24 @@
 function onButtonAddToWatched(selectedFilm) {
-    const filmsArray = [];
-    const currentFilmsWatched = localStorage.getItem('filmsWatched');
+  const filmsArray = [];
+  const currentFilmsWatched = localStorage.getItem('filmsWatched');
 
-    if (currentFilmsWatched) {
-        // console.log(
-        //   'уже есть массив с сохранёнными фильмами, добавляем объект с новым фильмом',
-        // );
+  if (currentFilmsWatched) {
+    // console.log(
+    //   'уже есть массив с сохранёнными фильмами, добавляем объект с новым фильмом',
+    // );
 
-        let filmsArray = JSON.parse(currentFilmsWatched);
+    let filmsArray = JSON.parse(currentFilmsWatched);
 
-        if (!filmsArray.find(({ id }) => id === selectedFilm.id)) {
-            filmsArray.push(selectedFilm);
-            localStorage.setItem('filmsWatched', JSON.stringify(filmsArray));
-        }
-    } else {
-        // console.log('массива с сохранёнными фильмами пока нет, создаём');
-
-        filmsArray.push(selectedFilm);
-        localStorage.setItem('filmsWatched', JSON.stringify(filmsArray));
+    if (!filmsArray.find(({ id }) => id === selectedFilm.id)) {
+      filmsArray.push(selectedFilm);
+      localStorage.setItem('filmsWatched', JSON.stringify(filmsArray));
     }
+  } else {
+    // console.log('массива с сохранёнными фильмами пока нет, создаём');
 
-    console.log(localStorage.getItem('filmsWatched'));
+    filmsArray.push(selectedFilm);
+    localStorage.setItem('filmsWatched', JSON.stringify(filmsArray));
+  }
 }
 
 export default onButtonAddToWatched;
