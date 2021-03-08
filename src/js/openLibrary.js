b0df6f6e5d9db83cedad962115555673c3a532fa
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-08 22:28:01 +0200

    added pagination func, removed old markup

--- src/js/openLibrary.js
+++ src/js/openLibrary.js
@@ -1,34 +1,36 @@
 import refs from './refs.js';
+import optionsPagination from '../js/paginationOptions';
 
 // modified by MAryasov
-import {libraryMarkup} from './components/library';
+import { libraryMarkup } from './components/library';
 // temporary data:
 const testData = {
-  cards:[
-      {
-        poster_path: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
-        title: "Titanic",
-        genre_ids: '18, 10749',        
-        release_date: "2010-08-07",
-        vote_average: 7.9
-      },
-      {
-        poster_path: "https://image.tmdb.org/t/p/original/e9XRikkyth0GtG8RkU3XNm0oMsA.jpg",
-        title: "Titanic II",
-        genre_ids: '28, 12, 53',        
-        release_date: "2010-08-07",
-        vote_average: 4.6
-      },
-      {
-        poster_path: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
-        title: "Titanic",
-        genre_ids: '18, 10749',        
-        release_date: "2010-08-07",
-        vote_average: 7.9
-      }
-  ]
-}
+  results: [
+    {
+      poster_path: '/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', //https://image.tmdb.org/t/p/original
+      title: 'Titanic',
+      genre_ids: '18, 10749',
+      release_date: '2010-08-07',
+      vote_average: 7.9,
+    },
+    {
+      poster_path: '/e9XRikkyth0GtG8RkU3XNm0oMsA.jpg',
+      title: 'Titanic II',
+      genre_ids: '28, 12, 53',
+      release_date: '2010-08-07',
+      vote_average: 4.6,
+    },
+    {
+      poster_path: '/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg',
+      title: 'Titanic',
+      genre_ids: '18, 10749',
+      release_date: '2010-08-07',
+      vote_average: 7.9,
+    },
+  ],
+};
 //
+const container = $('#library__page-selector');
 refs.navLibrary.addEventListener('click', openLibrary);
 
 function openLibrary(event) {
@@ -37,11 +39,45 @@ function openLibrary(event) {
   refs.homeGallery.classList.add('is-hidden');
   refs.buttons.classList.remove('is-hidden');
   // modified by MAryasov
-  refs.myLibraryGallery.textContent = '';
-  refs.myLibraryGallery.insertAdjacentHTML('afterbegin',libraryMarkup(testData).trim());
-  //
+  refs.libraryList.textContent = '';
+  container.pagination({
+    ...optionsPagination, //деструктуризация базовых настроек пагинатора (default options) рендер страницы зашит в дефолтных опциях!!!
+    dataSource: testData, //передача корня ссылки на сайт в данном случае ссылка поиска
+    pageSize: pageSizeCalc(window.innerWidth),
+    callback: function (data, pagination) {
+      // тут код с методами отрисовки макета страницы по шаблонам,
+      //data это кусок массива объектов согласно номера страницы dataSource[..., data<N- elements of array >, ...]
+      //------------------------------------------------
+      // pagination.pageSize = pageSizeCalc(window.innerWidth);
+      // console.log(pagination);
+      // console.log(data);
+      // homeTrending.page = Math.trunc(
+      //   Number(pagination.pageNumber) /
+      //     Math.trunc(20 / Number(pagination.pageSize)),
+      // );
+      // apiSearch.page = this.pageNumber;
+      //--------------------------------------------
+      const html = libraryMarkup(data);
+      $('#my-library').html(html);
+    },
+    // ajax: apiService.ajaxDataForSearch, // настройки запросов аякса под каждый сайт-сервер (apiKey,page,query)
+  });
+  // refs.myLibraryGallery.insertAdjacentHTML(
+  //   'afterbegin',
+  //   libraryMarkup(testData).trim(),
+  // );
+
   refs.myLibraryGallery.classList.remove('is-hidden');
-  refs.errorWarning.classList.add('is-hidden');  
+  refs.errorWarning.classList.add('is-hidden');
   refs.underscoreOnMyLibrary.classList.remove('is-hidden');
   refs.underscoreOnHome.classList.add('is-hidden');
+}
+function pageSizeCalc(innerWidth) {
+  if (innerWidth < 768) {
+    return 4;
+  }
+  if (innerWidth < 1024) {
+    return 8;
+  }
+  return 9;
 }
