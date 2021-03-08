8bc897956615b5275c3a635b9d4fd7148ac7c5de
Yuliana-vish <68988546+Yuliana-vish@users.noreply.github.com>
2021-03-08 09:09:18 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,16 @@
+const refs = {
+  logo: document.querySelector('.logo-icon'),
+  headerTitle: document.querySelector('.link-title'),
+  backgroundHome: document.querySelector('.header-home'),
+  gallery: document.querySelector('.movies-container'),
+  pagination: document.querySelector('.pagination'),
+  footer: document.querySelector('footer'),
+  myLibraryBtn: document.querySelector('.lib-link'),
+  inputForm: document.querySelector('.form-search'),
+  homeBtn: document.querySelector('.home-link'),
+  watchedBtn: document.querySelector('.button-watched'),
+  queueBtn: document.querySelector('.button-queque'),
+  body: document.querySelector('body'),
+};
+
+export default refs;
