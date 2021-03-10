f72b5ab583f45b57e7ca79cdaeb0c274eca853ce
AlexKmetyuk <kmetyuk.sasha@gmail.com>
2021-03-10 20:58:01 +0200

    pagination for queue and watched

--- src/js/fetchAPIandMovieList/fetchAPI.js
+++ src/js/fetchAPIandMovieList/fetchAPI.js
@@ -128,7 +128,7 @@ export default {
         }
       })
       .then(response => {
-        console.log(response);
+        // console.log(response);
         return response;
       })
       .catch(this.errorHandler);
