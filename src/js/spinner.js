e27c8a6312c4d1cf5c0dc8ce791071c6ab25e409
Yuliana-vish <68988546+Yuliana-vish@users.noreply.github.com>
2021-03-11 14:55:21 +0200

    Update spinner.js

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,9 +1,10 @@
-
-  export default {
-    show() {
-        document.querySelector('.spinner').classList.remove('is-hidden');
-    },
-    hide() {
-        document.querySelector('.spinner').classList.add('is-hidden');
-    },
-  };
+import refs from './refs.js';
+export default {
+  show() {
+    document.querySelector('.spinner').classList.remove('is-hidden');
+    refs.gallery.innerHTML = '';
+  },
+  hide() {
+    document.querySelector('.spinner').classList.add('is-hidden');
+  },
+};
