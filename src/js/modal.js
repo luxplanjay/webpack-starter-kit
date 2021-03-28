27f5dbe2c0d10114c495ba19c2cb132de4e03101
Max M <maxminullin@gmail.com>
2021-03-29 00:57:09 +0300

    addModal

---
+++ src/js/modal.js
@@ -0,0 +1,11 @@
+import refs from './refs';
+
+refs.gallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {}
+
+function openModal() {}
+
+function closeModal() {}
+
+function closeModalToPressEscape() {}
