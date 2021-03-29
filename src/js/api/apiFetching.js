4cc986d91ac2925ab8efbb9ca960475093c91dea
Maksym <maxthebest1906@gmail.com>
2021-03-29 19:55:32 +0300

    видалив мову

--- src/js/api/apiFetching.js
+++ src/js/api/apiFetching.js
@@ -1,17 +1,23 @@
 import variables from './apiVariables';
 
-const { BASE_URL, API_KEY, LANG } = variables;
+const { BASE_URL, API_KEY } = variables;
 
 const api = {
   fetchPopularFilms(page = '') {
-    const url = `${BASE_URL}/3/movie/popular?api_key=${API_KEY}&language=${LANG}&page=${page}`;
-    return fetch(url).then(rawData => {if(rawData.ok)  return rawData.json()})
-    .catch(() => console.error('no popular'));
+    const url = `${BASE_URL}/3/movie/popular?api_key=${API_KEY}&page=${page}`;
+    return fetch(url)
+      .then(rawData => {
+        if (rawData.ok) return rawData.json();
+      })
+      .catch(() => console.error('no popular'));
   },
   fetchFilmByQuery(page = '', searchQuery = '') {
-    const url = `${BASE_URL}/3/search/movie?api_key=${API_KEY}&language=${LANG}&query=${searchQuery}&page=${page}`;
-    return fetch(url).then(rawData => {if(rawData.ok)  return rawData.json()})
-    .catch(() => console.error('film is nod find!'));
+    const url = `${BASE_URL}/3/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}`;
+    return fetch(url)
+      .then(rawData => {
+        if (rawData.ok) return rawData.json();
+      })
+      .catch(() => console.error('film is nod find!'));
   },
 };
 
--- src/js/api/apiVariables.js
+++ src/js/api/apiVariables.js
@@ -1,5 +1,4 @@
 export default {
     BASE_URL: 'https://api.themoviedb.org',
     API_KEY: 'e0864e234ad59915a65f6e683712555d',
-    LANG: 'ru-Ru',
 };
