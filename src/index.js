000014b10c5dbb505251424427bcb9c29c3af4dc
Max M <maxminullin@gmail.com>
2021-03-29 16:14:40 +0300

    Базовые фн. модалки и рендер разметки в модалке

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './styles.scss';
+import './js/modal';
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,33 @@
 import refs from './refs';
+import renderCardFilmInModal from './fnRenderMarkupCardFilm'; //фн. создания разметки в модалке
 
-refs.gallery.addEventListener('click', onImageClick);
+refs.gallery.addEventListener('click', openModal);
+refs.btnClose.addEventListener("click", closeModal);
+refs.overlay.addEventListener('click', closeModal);
 
-function onImageClick(event) {}
+// function onImageClick(event) {}
 
-function openModal() {}
+function openModal() { //movieId
+      window.addEventListener('keydown', closeModalToPressEscape);
+      refs.modal.classList.add('is-open');
+      refs.body.classList.add('scroll-hidden');
 
-function closeModal() {}
+      refs.modalContent.innerHTML = '';
+      //тут будет функция
+      //fetch которая 
+      //возвращает промис с данными 
+      // о фильме и использует фн. renderCardFilmInModal(cardFilm) 
+}
 
-function closeModalToPressEscape() {}
+function closeModal() {
+      window.removeEventListener('keydown', closeModalToPressEscape);
+      refs.modal.classList.remove('is-open');
+      refs.body.classList.remove('scroll-hidden');
+      
+}
+
+function closeModalToPressEscape(event) {
+      if (event.code === 'Escape') {
+            closeModal();
+      }
+}
