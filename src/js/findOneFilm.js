201cfe28a413b4d360847db5498fb0dfc6cac109
Anton Kravchenko <lugagold@gmail.com>
2021-03-31 18:35:44 +0300

    Add Spinner to render Gallery and Modal
    Добавлен спинер при рендере Галереи и открытии модального окна карточки фильма.

--- src/js/findOneFilm.js
+++ src/js/findOneFilm.js
@@ -1,12 +1,11 @@
-import { Spinner } from 'spin.js';
-import opts from './spinner';
+import refs from './refs.js';
+import spinner from './spinner.js';
 
 const apiKey = "api_key=6df9a2b88a6cdc986e05b3daaeb09968";
 
 function fetchOneFilm(movie_id) {
     const url = `https://api.themoviedb.org/3/movie/${movie_id}?${apiKey}`;
-    let target = document.querySelector('.lightbox__content');
-    let spinner = new Spinner(opts).spin(target);
+    spinner.spin(refs.loadSpinner);
     return fetch(url)
         .then(response => response.json())
         .catch(error => console.log(error)).finally(() => {
