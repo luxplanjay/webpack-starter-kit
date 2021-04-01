25b8506ab36110559580eea114a70ac61bda0e1e
Oksana Chorna <ksuchorna@gmail.com>
2021-04-01 12:20:49 +0300

    Додано js файл для кнопки watch

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './js/apiService';
 import './js/search';
 import './js/renderGallery';
 import './js/modal';
-
+import './js/watched-btn';
---
+++ src/js/watched-btn.js
@@ -0,0 +1,44 @@
+const refs = {
+  watchedBtn: document.getElementById('watched-btn-js'),
+  queueBtn: document.getElementById('queue-btn-js'),
+  alertMessage: document.querySelector('.alert-mesage'),
+  libraryGallery: document.getElementById('js-library-gallery'),
+  //   libraryContainer: document.getElementById('js-library-container'),
+};
+
+refs.watchedBtn.addEventListener('click', e => {
+  e.preventDefault();
+  console.log(e.target.nodeName);
+  const watchedStr = localStorage.getItem('watched');
+  if (watchedStr === null) {
+    refs.alertMessage.innerHTML = '';
+
+    const messageSorry = document.createElement('p');
+    messageSorry.classList.add('library-message');
+    messageSorry.textContent = 'Soory, there are no movies';
+    refs.alertMessage.appendChild(messageSorry);
+  }
+  if (watchedStr) {
+    //вставити фільми у шаблон
+    try {
+      const dataLocalWatch = JSON.parse(watchedStr); // ''потрібні чи ні
+      console.log(dataLocalWatch);
+
+      //було так
+      // const createGalleryItem = ({ preview, original, description }) =>
+      //   `<li class="gallery__item"><a class="gallery__link" href='${original}' >
+      // <img class="gallery__image" src='${preview}' data-source='${original}'
+      // alt = '${description}' /></a ></li >`;
+      // взяти з хбс
+      // const galleryLibraryMarkup = dataLocalWatch.reduce(
+      //   (acc, item) => acc + createGalleryItem(item),
+      // де createGalleryItem шаблон строки лі
+      //   '',
+      // );
+
+      // refs.libraryGallery.insertAdjacentHTML("afterbegin", galleryLibraryMarkup);
+    } catch (error) {
+      console.log.error('Set state error: ', error);
+    }
+  }
+});
