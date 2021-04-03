5276e0e91d279abe866113041601d62077167b7c
Oleksandr_Gneushev <galexing@gmail.com>
2021-04-03 10:19:27 +0200

    preloader config

--- src/preloader/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function () {
-  const preloader = document.querySelector('.preloader');
-  document.body.removeChild(preloader);
-  document.getElementById('page-content').classList.add('ready');
-  console.log('done');
-});
