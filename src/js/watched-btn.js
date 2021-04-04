430153419afa4f755549f6e295804346480e38cc
Oksana Chorna <ksuchorna@gmail.com>
2021-04-04 22:07:13 +0300

    Підправлено

--- src/js/watched-btn.js
+++ src/js/watched-btn.js
@@ -6,28 +6,35 @@ const refs = {
   //   libraryContainer: document.getElementById('js-library-container'),
 };
 
-let watchedBtnActive = true;
-// якщо відкриваємо лібрарі, то вотчед активна, якщо клацаємо на кьюіє, то стає активною вона
-refs.watchedBtn.classList.add('controls-current');
+// якщо відкриваємо лібрарі, то watched активна, якщо клацаємо на queue, то стає активною вона
+refs.watchedBtn.classList.add('btn-active');
 const watchedStr = localStorage.getItem('watched');
 showWatchedMovies();
 
 refs.watchedBtn.addEventListener('click', e => {
   e.preventDefault();
-  // console.log(e.target.nodeName);
+
+  refs.queueBtn.classList.remove('btn-active');
+  refs.watchedBtn.classList.add('btn-active');
 
   showWatchedMovies();
+});
+
+refs.queueBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  refs.watchedBtn.classList.remove('btn-active');
+  refs.queueBtn.classList.add('btn-active');
+
+  showQueueMovies();
 });
 
 function showWatchedMovies() {
   if (watchedStr === null) {
     refs.alertMessage.innerHTML = '';
-
-    const messageSorry = document.createElement('p');
-    messageSorry.classList.add('library-message');
-    messageSorry.textContent = 'Soory, there are no movies';
-    refs.alertMessage.appendChild(messageSorry);
+    alertMessage();
   }
+  // watchedStr = '[{id: 1, src: ""}]';
   if (watchedStr) {
     //вставити фільми у шаблон
     try {
@@ -51,4 +58,43 @@ function showWatchedMovies() {
       console.log.error('Set state error: ', error);
     }
   }
+}
+
+const queueStr = localStorage.getItem('queue');
+
+function showQueueMovies() {
+  if (queueStr === null) {
+    refs.alertMessage.innerHTML = '';
+    alertMessage();
+  }
+  if (queueStr) {
+    //вставити фільми у шаблон
+    try {
+      const dataLocalqueue = JSON.parse(queueStr); // ''потрібні чи ні
+      console.log(dataLocalqueue);
+
+      //було так
+      // const createGalleryItem = ({ preview, original, description }) =>
+      //   `<li class="gallery__item"><a class="gallery__link" href='${original}' >
+      // <img class="gallery__image" src='${preview}' data-source='${original}'
+      // alt = '${description}' /></a ></li >`;
+      // взяти з хбс
+      // const galleryLibraryMarkup = dataLocalWatch.reduce(
+      //   (acc, item) => acc + createGalleryItem(item),
+      // де createGalleryItem шаблон строки лі
+      //   '',
+      // );
+
+      // refs.libraryGallery.insertAdjacentHTML("afterbegin", galleryLibraryMarkup);
+    } catch (error) {
+      console.log.error('Set state error: ', error);
+    }
+  }
+}
+
+function alertMessage() {
+  const messageSorry = document.createElement('p');
+  messageSorry.classList.add('library-message');
+  messageSorry.textContent = 'Soory, there are no movies';
+  refs.alertMessage.appendChild(messageSorry);
 }
