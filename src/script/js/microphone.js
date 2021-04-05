5850189d17712990f82b17072be46a0db4f88147
Oleksandr_Gneushev <galexing@gmail.com>
2021-04-05 20:56:40 +0200

    fix firefox issue

--- src/script/js/microphone.js
+++ src/script/js/microphone.js
@@ -1,44 +1,49 @@
 import refs from './refs';
 import hendlerInput from './search';
 
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+try {
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
+  const recognition = new SpeechRecognition();
+  recognition.lang = 'en-US';
+  recognition.interimResults = true;
 
-refs.microphone.addEventListener('click', clickOnMicrophone);
+  refs.microphone.addEventListener('click', clickOnMicrophone);
 
-function clickOnMicrophone() {
-  if (!refs.microphone.classList.contains('active-microphone')) {
-    refs.microphone.classList.add('active-microphone');
-    startRecognition();
-    return;
-  }
-  refs.microphone.classList.remove('active-microphone');
-  stopRecognition();
-}
-
-function listenSpeech(e) {
-  let transcript = Array.from(e.results)
-    .map(result => result[0].transcript)
-    .join('');
-  //  transcript = transcript[0].toUpperCase() + transcript.slice(1);
-  refs.searchInputRef.value = transcript;
-
-  if (e.results[0].isFinal) {
-    stopRecognition();
+  function clickOnMicrophone() {
+    if (!refs.microphone.classList.contains('active-microphone')) {
+      refs.microphone.classList.add('active-microphone');
+      startRecognition();
+      return;
+    }
     refs.microphone.classList.remove('active-microphone');
-    hendlerInput(transcript);
+    stopRecognition();
   }
-}
 
-function startRecognition() {
-  recognition.addEventListener('result', listenSpeech);
-  recognition.start();
-}
+  function listenSpeech(e) {
+    let transcript = Array.from(e.results)
+      .map(result => result[0].transcript)
+      .join('');
+    //  transcript = transcript[0].toUpperCase() + transcript.slice(1);
+    refs.searchInputRef.value = transcript;
 
-function stopRecognition() {
-  recognition.stop();
-  recognition.removeEventListener('result', listenSpeech);
+    if (e.results[0].isFinal) {
+      stopRecognition();
+      refs.microphone.classList.remove('active-microphone');
+      hendlerInput(transcript);
+    }
+  }
+
+  function startRecognition() {
+    recognition.addEventListener('result', listenSpeech);
+    recognition.start();
+  }
+
+  function stopRecognition() {
+    recognition.stop();
+    recognition.removeEventListener('result', listenSpeech);
+  }
+} catch {
+  refs.microphone.style.display = 'none';
 }
