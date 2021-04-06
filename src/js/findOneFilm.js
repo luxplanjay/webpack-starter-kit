45d7f8cba34c6a875f9cad803a6eb14436a31aab
Max M <maxminullin@gmail.com>
2021-04-06 23:16:55 +0300

    fix_spinner_in_modal

--- src/js/findOneFilm.js
+++ src/js/findOneFilm.js
@@ -7,10 +7,14 @@ const apiKey = "6df9a2b88a6cdc986e05b3daaeb09968";
 function fetchOneFilm(movie_id) {
     spinner.spin(refs.loadSpinner);
     return fetch(`${BASE_URL}/${movie_id}?api_key=${apiKey}&language=en-US`)
-        .then(response => {
-            if (!response.ok) return Promise.reject('Server error!');
-            return response.json();
-            });
+        .then(response => response.json())
+        .catch(error => console.log(error))
+        .finally(() => {
+            spinner.stop();
+        });
 }
+            // if (!response.ok) return Promise.reject('Server error!');
+            // return response.json();
+            // }
 
 export default fetchOneFilm;
