0b5e603ccca0298e3c48ce6b40d6dcbd8eb5e55d
Oksana Chorna <ksuchorna@gmail.com>
2021-04-07 20:38:29 +0300

    Changes

--- src/js/watched-btn.js
+++ src/js/watched-btn.js
@@ -5,63 +5,72 @@ const refs = {
   libraryGallery: document.getElementById('js-library-gallery'),
   //   libraryContainer: document.getElementById('js-library-container'),
 };
-
+import movieGallaryCardTmpl from '../tamplates/movie-gallery-card.hbs';
+ 
 // якщо відкриваємо лібрарі, то watched активна,
 refs.watchedBtn.classList.add('btn-active');
 const watchedStr = localStorage.getItem('watched');
 showWatchedMovies();
-
+ 
 refs.watchedBtn.addEventListener('click', e => {
   e.preventDefault();
-
+ 
   refs.queueBtn.classList.remove('btn-active');
   refs.watchedBtn.classList.add('btn-active');
-
+ 
   showWatchedMovies();
 });
-
-const queueStr = localStorage.getItem('queue');
+ 
 // якщо клацаємо на queue, то стає активною вона
+const queueStr = localStorage.getItem('queue');
+ 
 refs.queueBtn.addEventListener('click', e => {
   e.preventDefault();
-
+ 
   refs.watchedBtn.classList.remove('btn-active');
   refs.queueBtn.classList.add('btn-active');
-
+ 
   showQueueMovies();
 });
-
+ 
 function showWatchedMovies() {
+  // якщо localStorage пустий, то показуємо повідомлення
   if (watchedStr === null) {
     refs.alertMessage.innerHTML = '';
     alertMessage();
   }
-  // watchedStr = '[{id: 1, src: ""}]';
+  // по ключу показуємо фільми (watchedStr = '[{id: 1, src: ""}]';)
   if (watchedStr) {
     //вставити фільми у шаблон
     try {
       const dataLocalWatch = JSON.parse(watchedStr); // ''потрібні чи ні
       console.log(dataLocalWatch);
-
-      //було так
-      // const createGalleryItem = ({ preview, original, description }) =>
-      //   `<li class="gallery__item"><a class="gallery__link" href='${original}' >
+      // варіант 1
+      // const createGalleryItem = ({ { id, poster_path, title,
+                        // release_date, genre_ids,
+                        // vote_average, vote_count, original_title,
+                        // genres, overview, popularity } }) =>
+      //   (`<li class="gallery__item"><a class="gallery__link" href='${original}' >
       // <img class="gallery__image" src='${preview}' data-source='${original}'
-      // alt = '${description}' /></a ></li >`;
-      // взяти з хбс
+      // alt = '${description}' /></a ></li >`;) - замість цього темплейт
+      // взяти з хбс шаблон
+      //const movieTmptwithDestr = movieGallaryCardTmpl(createGalleryItem);
+      // варіант 2
       // const galleryLibraryMarkup = dataLocalWatch.reduce(
-      //   (acc, item) => acc + createGalleryItem(item),
+      //   (acc, item) => acc + createGalleryItem(item), 
+      // чи // (acc, item) => acc + movieGallaryCardTmpl(item),
       // де createGalleryItem шаблон строки лі
       //   '',
       // );
-
-      // refs.libraryGallery.insertAdjacentHTML("afterbegin", galleryLibraryMarkup);
+      // варіант 3
+      // const galleryLibraryMarkup = movieGallaryCardTmpl(dataLocalWatch);
+      // refs.libraryGallery.insertAdjacentHTML("beforeend", galleryLibraryMarkup);
     } catch (error) {
       console.log.error('Set state error: ', error);
     }
   }
 }
-
+ 
 function showQueueMovies() {
   if (queueStr === null) {
     refs.alertMessage.innerHTML = '';
@@ -72,7 +81,7 @@ function showQueueMovies() {
     try {
       const dataLocalqueue = JSON.parse(queueStr); // ''потрібні чи ні
       console.log(dataLocalqueue);
-
+ 
       //було так
       // const createGalleryItem = ({ preview, original, description }) =>
       //   `<li class="gallery__item"><a class="gallery__link" href='${original}' >
@@ -84,17 +93,18 @@ function showQueueMovies() {
       // де createGalleryItem шаблон строки лі
       //   '',
       // );
-
+ 
       // refs.libraryGallery.insertAdjacentHTML("afterbegin", galleryLibraryMarkup);
     } catch (error) {
       console.log.error('Set state error: ', error);
     }
   }
 }
-
+ 
 function alertMessage() {
   const messageSorry = document.createElement('p');
   messageSorry.classList.add('library-message');
   messageSorry.textContent = 'Sorry, there are no movies';
   refs.alertMessage.appendChild(messageSorry);
 }
+ 
